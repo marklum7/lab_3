ed7c2542cec9b68a5f06c0da8b7349d4e38ca199
marklum7 <122746275+marklum7@users.noreply.github.com>
2023-05-29 00:33:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; ++i) {
+            System.out.println("Таблица умножения на " + i);
+
+            for(int j = 1; j <= 10; ++j) {
+                System.out.println("" + i + "*" + j + "=" + j * i);
+            }
+        }
+
+    }
+}
